dd10e22c6cf4fb7fe261d196784094e18affc13c
Gabriel Viana <gabrielviana@MacBook-Pro-de-Gabriel.local>
2023-06-17 13:46:55 -0300

    alterações

--- src/server.js
+++ src/server.js
@@ -2,12 +2,14 @@ require('express-async-errors');
  const AppError = require('./utils/AppError');
 
  const express = require('express');
+ const cors = require('cors')
  const app = express();
 
  const uploadConfig = require('./configs/upload');
 
  const routes = require('./routes');
  app.use(express.json());
+ app.use(cors());
  app.use('/files', express.static(uploadConfig.UPLOADS_FOLDER));
  app.use(routes);
 
